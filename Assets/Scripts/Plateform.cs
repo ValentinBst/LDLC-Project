c8f9860a5b90fbfe10d682ff98738c55cb0513d3
LAPTOP-0B4K03HR\Valentin Bst <briset.v@gmail.com>
2020-11-27 10:14:41 +0100

    J'index.lol

--- Assets/Scripts/Plateform.cs
+++ Assets/Scripts/Plateform.cs
@@ -4,17 +4,27 @@ using UnityEngine;
 
 public class Plateform : MonoBehaviour
 {
+    [SerializeField]
+    Vector3 positionDeDepart;
+    float Vitesse;
     Rigidbody rigidbody;
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
     }
-
+    Vector3 direction = new Vector3(0f, 0f, 1f);
     void FixedUpdate()
     {
         Vector3 currentPosition = transform.position;
-        Vector3 direction = new Vector3(0f, 0f, 1f);
         float speed = 2f;
+        if (currentPosition.z > 9)
+        {
+            direction = new Vector3(0F, 0F, -1f);
+        }
+        if (currentPosition.z < -4)
+        {
+            direction = new Vector3(0F, 0F, 1f);
+        }
 
         Vector3 newPosition = currentPosition + direction * speed * Time.deltaTime;
 
