56428601739ac0922e04118a3c35b0ebd3dbf0ac
LAPTOP-0B4K03HR\Valentin Bst <briset.v@gmail.com>
2020-11-30 11:34:28 +0100

    Avancement incroyable

---
+++ Assets/Scripts/Balle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Sphere;
+
+
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetDir = transform.forward;
+    }
+}
--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -2,13 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Shoot : MonoBehaviour;
-{
-    [SerializeField]
-    GameObject sphere;
-    
+public class Shoot : MonoBehaviour {
+public Rigidbody projectile;
+public Transform Spawnpoint;
+
+   
+    public GameObject Sphere;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
     void Update()
     {
-        transform.forward;
+        if (Input.GetMouseButtonDown(0))
+        {
+            Rigidbody clone;
+            clone = (Rigidbody)Instantiate(projectile, Spawnpoint.position, projectile.rotation);
+
+            clone.velocity = Spawnpoint.TransformDirection(Vector3.forward * 60);
+        }
     }
 }
